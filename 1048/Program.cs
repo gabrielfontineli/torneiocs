567c239e700e37995116d4459fdc7415e42ebad5
fonti <gabriel0fontineli@gmail.com>
2021-02-01 14:20:29 -0300

    final?

--- 1048/Program.cs
+++ 1048/Program.cs
@@ -6,7 +6,40 @@ namespace _1048
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+    double s = double.Parse(Console.ReadLine());
+    double t = 0;
+
+    if (s < 400.01) {
+      t = s + s * 0.15;
+      Console.WriteLine($"Novo salario: {t:.00}");
+      Console.WriteLine($"Reajuste ganho: {s * 0.15:.00}");
+      Console.WriteLine("Em percentual: 15 %");
+    }
+    if (400.00 < s && s < 800.01) {
+      t = s + s * 0.12;
+      Console.WriteLine($"Novo salario: {t:.00}");
+      Console.WriteLine($"Reajuste ganho: {s * 0.12:.00}");
+      Console.WriteLine("Em percentual: 12 %");
+    }
+    if (800.00 < s && s < 1200.01) {
+      t = s + s * 0.1;
+      Console.WriteLine($"Novo salario: {t:.00}");
+      Console.WriteLine($"Reajuste ganho: {s * 0.1:.00}");
+      Console.WriteLine("Em percentual: 10 %");
+    }
+    if (1200.00 < s && s < 2000.01) {
+      t = s + s * 0.07;
+      Console.WriteLine($"Novo salario: {t:.00}");
+      Console.WriteLine($"Reajuste ganho: {s * 0.07:.00}");
+      Console.WriteLine("Em percentual: 7 %");
+    }
+    if (2000.00 < s) {
+      t = s + s * 0.04;
+      Console.WriteLine($"Novo salario: {t:.00}");
+      Console.WriteLine($"Reajuste ganho: {s * 0.04:.00}");
+      Console.WriteLine("Em percentual: 4 %");
+    }
+            
         }
     }
 }
---
+++ 1091/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1091
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                      while(true){
+                int k,n,m,x,y;
+                k = int.Parse(Console.ReadLine());
+                if (k==0) break;
+                string[] ent = Console.ReadLine().Split();
+                n = int.Parse(ent[0]);
+                m = int.Parse(ent[1]);
+
+                for(int i = 0; i<k;i++){
+                    string[] cord = Console.ReadLine().Split();
+                    x = int.Parse(cord[0]);y = int.Parse(cord[1]);
+
+                    if(x==n ||y==m) Console.WriteLine("divisa");
+                    else if(x > n && y > m) Console.WriteLine("NE");
+                    else if(x > n && y < m) Console.WriteLine("SE");
+                    else if(x < n && y > m) Console.WriteLine("NO");
+                    else if(x <n && y <m)Console.WriteLine("SO");
+}}
+        }
+    }
+}
